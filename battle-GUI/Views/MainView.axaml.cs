c458aded7967ebb3bf5aa5c70be4eac987797aea
Didi5GD <DidiFon5GD@gmail.com>
2025-02-06 19:44:52 +0700

    started making grid

--- battle-GUI/Views/MainView.axaml.cs
+++ battle-GUI/Views/MainView.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace battle_GUI.Views;
 
@@ -9,8 +10,35 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
     }
-    public void ClickHandler(object sender, RoutedEventArgs args)
+
+    private void OnButtonClick(object sender, RoutedEventArgs e)
     {
-   
+        // Изменяем цвет фона кнопки
+        var button = sender as Button;
+        var act = sender as RadioButton() ;
+        if (button != null)
+        {
+            if (act != null) {
+                act.Go
+
+
+
+
+            button.Background = Brushes.Pink; // Установите нужный цвет   // Находим родительский Grid
+                var parentGrid = (Grid)button.Parent;
+                // Получаем строку и столбец кнопки
+                int row = Grid.GetRow(button);
+                int column = Grid.GetColumn(button);
+                // Теперь у Вас есть координаты
+
+
+
+
+
+            }
+        }
     }
+
+  
+   
 }
