83ec5c246e1c7e423cab4a90037518953143be66
Didi5GD <DidiFon5GD@gmail.com>
2025-02-06 11:49:16 +0700

    made grid

--- battle-GUI/Views/MainView.axaml.cs
+++ battle-GUI/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace battle_GUI.Views;
 
@@ -7,5 +8,9 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+    public void ClickHandler(object sender, RoutedEventArgs args)
+    {
+   
     }
 }
